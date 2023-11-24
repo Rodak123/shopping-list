95a6ac26d3cc5e5a055bae9e5ffe5a522a74867d
Rodak123 <rodak.titera@gmail.com>
2023-11-24 16:18:34 +0100

    API refreshes

--- src/contexts/ApiContext.js
+++ src/contexts/ApiContext.js
@@ -4,40 +4,44 @@ import axios from 'axios';
 const ApiContext = createContext();
 
 export const ApiProvider = ({ children }) => {
-  const [api, setApi] = useState(null);
-  const [apiLoaded, setApiLoaded] = useState(false);
+    const [api, setApi] = useState(null);
+    const [apiLoaded, setApiLoaded] = useState(false);
 
-  useEffect(() => {
-    if (api === null) {
-      const id = 1;
-      const api = {
-        url: 'http://localhost:3100',
-        id: id,
-      };
-      setApi(api);
-    }
-  }, [api]);
+    const loadApi = () => {
+        axios
+            .get(api.url + '/user/' + api.id)
+            .then(function (res) {
+                if (res.data) {
+                    setApiLoaded(true);
+                    api.id = res.data.id;
+                }
+            })
+            .catch(function (error) {
+                setApiLoaded(false);
+                setTimeout(loadApi, 5000);
+            });
+    };
 
-  useEffect(() => {
-    if (apiLoaded === false && api !== null) {
-      axios
-        .get(api.url + '/user/' + api.id)
-        .then(function (res) {
-          if (res.data) {
-            setApiLoaded(true);
-            api.id = res.data.id;
-          }
-        })
-        .catch(function (error) {
-          setApiLoaded(false);
-          console.log(error);
-        });
-    }
-  }, [apiLoaded, api]);
+    useEffect(() => {
+        if (api === null) {
+            const id = 1;
+            const api = {
+                url: 'http://localhost:3100',
+                id: id,
+            };
+            setApi(api);
+        }
+    }, [api]);
 
-  return <ApiContext.Provider value={{ api, apiLoaded }}>{children}</ApiContext.Provider>;
+    useEffect(() => {
+        if (apiLoaded === false && api !== null) {
+            loadApi();
+        }
+    }, [apiLoaded, api, loadApi]);
+
+    return <ApiContext.Provider value={{ api, apiLoaded }}>{children}</ApiContext.Provider>;
 };
 
 export const useApi = () => {
-  return useContext(ApiContext);
+    return useContext(ApiContext);
 };
